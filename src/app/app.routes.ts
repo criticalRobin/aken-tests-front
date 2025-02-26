beb53b0badb69c56d14943787139483b92a963a2
criticalRobin <matiastite1@gmail.com>
2025-02-26 17:21:15 -0500

    Creado proyecto Angular

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'test',
+    loadComponent: () =>
+      import('./components/comp-route/comp-route.component').then(
+        (c) => c.CompRouteComponent
+      ),
+  },
+];
